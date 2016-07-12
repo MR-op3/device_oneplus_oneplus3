0be014b2877502e7c7947f6ec23b11a59e124480
Martinusbe <martinusbe@gmail.com>
2016-07-12 11:12:08 +0200

    mr: make it show installed recovery in manager
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -255,9 +255,11 @@ DEVICE_RESOLUTION := 1080x1920
 MR_PIXEL_FORMAT := "RGBA_8888"
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 MR_DEV_BLOCK_BOOTDEVICE := true
-
-MR_DEVICE_SPECIFIC_VERSION := b
+# Versioning
 include $(PLATFORM_PATH)/multirom/MR_REC_VERSION.mk
+BOARD_MKBOOTIMG_ARGS += --board mrom$(MR_REC_VERSION)
+MR_REC_VERSION := $(shell date -u +%Y%m%d)-01
+MR_DEVICE_SPECIFIC_VERSION := b
 
 #Force populating /dev/block/platform/msm_sdcc.1/by-name
 #from the emmc
