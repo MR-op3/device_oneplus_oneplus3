dd8ec42cbe8bdee8caa8efd0d4170bbaa3486a09
Martinusbe <martinusbe@gmail.com>
2016-11-25 15:20:58 +0100

    op3: update for 7.x
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -234,7 +234,7 @@ TWHAVE_SELINUX := true
 
 # MR config. MultiROM also uses parts of TWRP config
 TARGET_RECOVERY_IS_MULTIROM := true
-MR_ALLOW_NKK71_NOKEXEC_WORKAROUND := true
+MR_NO_KEXEC := enabled
 MR_CONTINUOUS_FB_UPDATE := true
 MR_DPI := xhdpi
 MR_DPI_FONT := 340
@@ -242,11 +242,11 @@ MR_USE_MROM_FSTAB := true
 MR_FSTAB := $(PLATFORM_PATH)/multirom/mrom.fstab
 MR_INPUT_TYPE := type_b
 MR_INIT_DEVICES := $(PLATFORM_PATH)/multirom/mr_init_devices.c
-MR_KEXEC_MEM_MIN := 0x00200000
+MR_KEXEC_MEM_MIN := 0xac000000
 MR_KEXEC_DTB := true
 MR_DEVICE_HOOKS := $(PLATFORM_PATH)/multirom/mr_hooks.c
 MR_DEVICE_HOOKS_VER := 4
-MR_DEVICE_VARIANTS := OnePlus3
+MR_DEVICE_VARIANTS := OnePlus3 oneplus3
 MR_USE_QCOM_OVERLAY := true
 MR_QCOM_OVERLAY_HEADER := $(PLATFORM_PATH)/multirom/mr_qcom_overlay.h
 MR_QCOM_OVERLAY_CUSTOM_PIXEL_FORMAT := MDP_RGBX_8888
@@ -259,7 +259,8 @@ MR_DEV_BLOCK_BOOTDEVICE := true
 include $(PLATFORM_PATH)/multirom/MR_REC_VERSION.mk
 BOARD_MKBOOTIMG_ARGS += --board mrom$(MR_REC_VERSION)
 MR_REC_VERSION := $(shell date -u +%Y%m%d)-01
-MR_DEVICE_SPECIFIC_VERSION := b
+MR_DEVICE_SPECIFIC_VERSION := e
+MR_ENCRYPTION := true
 
 #Force populating /dev/block/platform/msm_sdcc.1/by-name
 #from the emmc
